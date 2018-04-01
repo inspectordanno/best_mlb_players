e0ba77921b0504efa1a35a484413c1c66641d9fc
Dan Spector <danno2423@gmail.com>
2018-03-31 21:10:37 -0400

    "in class work"

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,13 +1,31 @@
 import {csv} from 'd3';
 
-export const fetchCsv = (url, parse) => {
-	return new Promise((resolve, reject) => {
-		csv(url, parse, (err, data) => {
-			if(err){
-				reject(err);
-			}else{
-				resolve(data);
-			}
-		})
-	});
-}
+export const parse = d => {
+	return {
+		Batting_Average: +d.BA,
+		ERA: +d.ERA,
+		Home_Runs: +d.HR,
+		Hits: +d.Hits,
+		K_Per_Nine: +d.K_9,
+		Losses: +d.L,
+		RBI: +d.RBI,
+		Runs: +d.R,
+		Stolen_Bases: +d.SB,
+		Strike_Outs: +d.SO,
+		Wins: +d.W,
+		Wins_Above_Replacement: +d.WAR,
+		WHIP: +d.WHIP
+	};
+};
+
+// export const fetchCsv = (url, parse) => {
+// 	return new Promise((resolve, reject) => {
+// 		csv(url, parse, (err, data) => {
+// 			if(err){
+// 				reject(err);
+// 			}else{
+// 				resolve(data);
+// 			}
+// 		})
+// 	});
+// }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,36 @@ console.log('index.js!');
 import * as d3 from 'd3';
 import * as jquery from 'jquery';
 import './style/appStyles.scss';
-import fetchCsv from './components/utils'
+import {parse} from './components/utils'
 
-//Import data using the Promise interface
-Promise.all([
-  fetchCsv('../dist/data/baseball.csv')])
-  .then(([baseball]) => {
-    console.log(baseball);
+// Import data using the Promise interface
+// Promise.all([
+//   fetchCsv('./data/baseball.csv')])
+//   .then(([baseball]) => {
+//     console.log(baseball);
+//   });
+
+  d3.csv('./data/baseball.csv',parse).then(function(data) {
+    console.log(data); // [{"Hello": "world"}, …]
   });
+
+
+  //scale svg_container
+
+
+  const svgBaseballField = document.getElementById('diamond');
+  const svgLayer = document.getElementById('layer1');
+  console.log(svgBaseballField);
+  console.log(svgLayer);
+  // const svgContainer = document.getElementsByClassName('svg_container')[0];
+  // console.log(svgContainer);
+  //
+  // const svgContainerHeight = svgContainer.clientHeight;
+  // console.log(svgContainerHeight);
+  // const svgContainerWidth = svgContainer.clientWidth;
+  // console.log(svgContainerWidth);
+
+  svgBaseballField.setAttribute("width", '100%');
+  svgBaseballField.setAttribute("height", '100%');
+
+  svgBaseballField.setAttribute("viewBox", "0 0 200 200");
