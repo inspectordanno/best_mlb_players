c8305172428cd3e50eaa717e213108f78fb153d3
Dan Spector <danno2423@gmail.com>
2018-04-03 20:16:01 -0400

    "changed parse function and svg id names"

--- src/components/utils.js
+++ src/components/utils.js
@@ -2,6 +2,10 @@ import {csv} from 'd3';
 
 export const parse = d => {
 	return {
+		Year: d.Year,
+		Position: d.Position,
+		Name: d.Name,
+		Teams: d.Teams,
 		Batting_Average: +d.BA,
 		ERA: +d.ERA,
 		Home_Runs: +d.HR,
@@ -9,7 +13,7 @@ export const parse = d => {
 		K_Per_Nine: +d.K_9,
 		Losses: +d.L,
 		RBI: +d.RBI,
-		Runs: +d.R,
+		Runs: +d.Runs,
 		Stolen_Bases: +d.SB,
 		Strike_Outs: +d.SO,
 		Wins: +d.W,
