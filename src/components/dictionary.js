8dd337e02e07f337efcb22f1092fbe65245fc0dc
Dan Spector <danno2423@gmail.com>
2018-04-19 12:26:49 -0400

    "changed dictionary"

--- src/components/dictionary.js
+++ src/components/dictionary.js
@@ -3,8 +3,8 @@ import {map, select} from 'd3';
 const dictionary = map();
         dictionary.set('Pitcher', select('#pitcher').node().getBoundingClientRect()); //pitcher;
 
-        console.log(dictionary)
-        //export const dictionary;
+        console.log(dictionary);
+        export const dictionary;
 
   // .set('Left Field') //left field
   // .set('Center Field')//center field
