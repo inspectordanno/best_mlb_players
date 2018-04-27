e17b763010e965917430c7069bdb845cded5ab78
Dan Spector <danno2423@gmail.com>
2018-04-27 18:36:30 -0400

    "npm run build"

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,6 +2,7 @@ const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = merge(common, {
 	plugins:[
