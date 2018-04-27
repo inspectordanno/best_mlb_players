f1302b715cb9268146d2a276e90d57ace178bc9b
Dan Spector <danno2423@gmail.com>
2018-04-27 12:14:12 -0400

    "fixed enter update exit"

--- src/components/players.js
+++ src/components/players.js
@@ -1,5 +1,5 @@
 import * as d3 from 'd3';
-import {positionDictionary, colorDictionary} from './dictionary';
+import {positionDictionary, colorDictionary, fieldDictionary} from './dictionary';
 
 export default function(players){
 
@@ -9,45 +9,66 @@ export default function(players){
 
   const playersNode = d3.select('#layer1')
     .selectAll('.player')
-    .data(players, d => d.Name);
+    .data(players, d => {
+      return d.Name;
+    });
+
+    playersNode.transition()
+    // .delay(100)
+    .attr('stroke', 'teal');
+
+  console.log(playersNode);
 
   console.log(positionDictionary);
 
   playersNode.enter().append('circle')
     .merge(playersNode)
-    .attr('class','player')
-    .attr('id', d => d.Name)
-    .attr('cx', d => {
-      const xy = positionDictionary.get(d.Position);
-      if(!xy) return 0;
-      console.log(xy);
-      // console.log(d3.select("svg").node().getBBox().width / 650);
-      // return xy.x * (d3.select("svg").node().getBBox().width / 650);
-      return xy.x;
+    .attr('class', d => {
+      const fillColor = colorDictionary.get(d.Year);
+      return fillColor + ' player';
     })
+    .attr('id', d => d.Name)
+    .attr("cx",0)
+    .attr("cy",0)
+    .transition()
+    .attr('cx', d => {
+    const xy = positionDictionary.get(d.Position);
+    if(!xy) return 0;
+    console.log(xy);
+    // console.log(d3.select("svg").node().getBBox().width / 650);
+    // return xy.x * (d3.select("svg").node().getBBox().width / 650);
+    return xy.x;
+  })
     .attr('cy', d => {
       const xy = positionDictionary.get(d.Position);
       if(!xy) return 0;
       return xy.y;
     })
-    // .style('fill', d => {
-    //   const colorClass = colorDictionary.get(d.Year);
-    //   console.log( `var(${colorClass})`);
-    //   return `var(${colorClass})`;
-    // })
-    .attr('class', d => {
-      const fillColor = colorDictionary.get(d.Year);
-      return fillColor;
+    .style('fill', d => {
+      const colorClass = colorDictionary.get(d.Year);
+      console.log( `var(${colorClass})`);
+      return `var(${colorClass})`;
     })
-    .attr('r', 8)
-    .on("mouseover", d => {
-      d3.select(".player_wrapper").html(d.Name);
-      console.log("hi");
+    .attr('r', 8);
+
+
+    d3.selectAll('.player').on("mouseover", d => {
+      console.log(d3.select("span#insert"))
+      d3.select("#insert").html(d.R);
+      d3.select('#title_name')
+        .style('color', fieldDictionary.get(d.Year));
+      console.log(d.R);
     });
+
+    playersNode.exit()
+      .transition()
+      .attr("cx",0)
+      .attr("cy",0)
+      .remove();
 
   console.log(playersNode);
   console.log(playersNode.enter())
   console.log(playersNode.exit());
-  playersNode.exit().remove();
+
 
 };
